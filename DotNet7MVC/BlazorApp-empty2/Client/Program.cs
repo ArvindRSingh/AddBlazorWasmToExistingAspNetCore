91a5ea770e2acd5e6a9433d073765534a7e41f6b
Arvind Singh <ArvindRSingh@nivra.net>
2023-03-18 23:28:47 +0530

    Blazor WASM added to ASP.NET Core MVC

---
+++ DotNet7MVC/BlazorApp-empty2/Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp_empty2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorApp_empty2.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorApp_empty2.ServerAPI"));
+
+await builder.Build().RunAsync();
